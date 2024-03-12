ceec6e642a3afbacdb5f5eab5ad9576ca8df9dc9
iSaKaaaaaaa <phawinkaka@gmail.com>
2024-03-12 14:08:13 +0700

    Update Book.java

--- ShowRoom/src/Book.java
+++ ShowRoom/src/Book.java
@@ -1,3 +1,23 @@
 public class Book {
-   
+   private int bookingid;
+   private Car car;
+   private Customer customer;
+   private Date bookingDate;
+
+   public int getBookingid(){
+
+   }
+
+   public Car getCar(){
+
+   }
+
+   public Customer getCustomer(){
+
+   }
+
+   public Date getBookingDate(){
+
+   }
+
 }
