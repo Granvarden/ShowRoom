e7927eb32d739909f44c14432135aa71900f3938
SasakiYumeko <66070241@kmitl.ac.th>
2024-03-13 21:56:17 +0700

    admin#2

--- ShowRoom/src/Admin.java
+++ ShowRoom/src/Admin.java
@@ -1,35 +1,36 @@
 public class Admin {
+    
     private String username;
     private String password;
 
-    public AdminImpl(String username, String password) {
+    public Admin(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
-    @Override
+    
     public void addCar(Car car) {
         
     }
 
-    @Override
+    
     public void removeCar(Car car) {
         
     }
 
-    @Override
+   
     public void assignEmployee(Employee e) {
         
     }
 
-     @Override
+    
     public String showDetailsEmployee(Employee e) {
         String employeeName = e.getName();
         
         return "Employee Name: " + employeeName;
     }
 
-    @Override
+    
     public String showDetailsCustomer(Customer c) {
         String customerName = c.getName();
         String phoneNumber = c.getPhoneNumber();
@@ -40,8 +41,8 @@ public class Admin {
                ", Email: " + email;                
     }
 
-    @Override
-    public void cancelBooking(Car booking) {
+    
+    public void cancelBooking(Book booking) {
         Car car_1 = booking.getCar();
         car_1.cancelBooking();
     }
