fceb057dcaddd1d58c99de5445d149bf44fd4cd3
TunaOfFinality <66070071@kmitl.ac.th>
2024-03-13 22:24:35 +0700

    Update showDetails()

--- ShowRoom/src/SportCar.java
+++ ShowRoom/src/SportCar.java
@@ -1,17 +1,17 @@
 public class SportCar extends Car{
-    private String type;
-    
-    protected void setType(String type) {
-        this.type = type;
+    public SportCar(String name, int cc, int price, String type, boolean booked) {
+        super(name, cc, price, type, booked);
     }
-    
-    protected String getType() {
-        return this.type;
-    }
-    
-//    @Override
-//    NOT FINISHED
+    @Override
     public void showDetails() {
-        System.out.println("...................");
+        System.out.println("Car name: " + super.getName());
+        System.out.println("Type: " + super.getType());
+        System.out.println("CC: " + super.getCC());
+        if(this.isBooked() == true) {
+            System.out.println("The car is booked.");
+        } else {
+            System.out.println("The car is not booked");
+        }
+        System.out.println("Price: " + super.getPrice());
     }
 }
