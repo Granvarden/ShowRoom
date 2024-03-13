17cdb45fb46a61e1f317ba46ba3ee08abcebee87
Tadaphat9 <miss.sunisa1978@gmail.com>
2024-03-13 17:41:20 +0700

    Update FinanceCompany.java

--- ShowRoom/src/FinanceCompany.java
+++ ShowRoom/src/FinanceCompany.java
@@ -1,3 +1,17 @@
 public class FinanceCompany {
+    private String name;
+
+    // Constructor Method
+    public FinanceCompany(String name) {
+        this.name = name;
+    }
     
+    // Plan buy car
+    public double plan_A() {
+       
+    }
+    
+    public double plan_B() {
+        
+    }
 }
