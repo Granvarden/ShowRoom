2a48d1d8103e0b47ce57a591a36f82292a14fed5
66070049 <66070049@kmitl.ac.th>
2024-03-13 21:24:08 +0700

    Update Vehicle & Car

--- ShowRoom/src/Car.java
+++ ShowRoom/src/Car.java
@@ -1,3 +1,23 @@
 public abstract class Car extends Vehicle{
-    
+    private String type;
+    private boolean booked;
+    protected void setType(String type){
+        this.type = type;
+    }
+    protected String getType(){
+        return type;
+    }
+    public void book(){
+        this.booked = true;
+    }
+    public  void cancleBooking(){
+        this.booked = false;
+    }
+    @Override
+    public void showDetails(){
+        System.out.println("A car type is " + type + ".");
+    }
+    public boolean isBooked(){
+        return booked;
+    }  
 }
--- ShowRoom/src/Vehicle.java
+++ ShowRoom/src/Vehicle.java
@@ -1,3 +1,26 @@
 public abstract class Vehicle {
-    
+    private String name;
+    private int cc;
+    private int price;
+    protected void setName(String name){
+        this.name = name;
+    }
+    protected String getName(){
+        return name;
+    }
+    protected void setCC(int cc){
+        this.cc = cc;
+    }
+    protected int getCC(){
+        return cc;
+    }
+    protected void setPrice(int price){
+        this.price = price;
+    }
+    protected int getPrice(){
+        return price;
+    }
+    public void showDetails(){
+        System.out.println(name + "have a " + cc + " cc.");
+    }
 }
