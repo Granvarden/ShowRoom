23a70ef8cb1eed126d00f5887e4691c235b0fedb
Tadaphat9 <miss.sunisa1978@gmail.com>
2024-03-13 18:36:48 +0700

    Update FinanceCompany.java

--- ShowRoom/src/FinanceCompany.java
+++ ShowRoom/src/FinanceCompany.java
@@ -7,11 +7,21 @@ public class FinanceCompany {
     }
     
     // Plan buy car
-    public double plan_A() {
-       
-    }
-    
-    public double plan_B() {
+    public double plan_A(double loanAmount) {
+        double interestRate = 0.07; // rate 7% 
+        int loanTerm = 4; // Or 48 months loan term
+        double totalInterest = (loanAmount * interestRate) * loanTerm;
+        double totalPayment = loanAmount + totalInterest;
+        // double payPerMonth = totalPayment / (loanTerm * 12);
         
+        return totalPayment;
+    }
+    public double plan_B(double loanAmount, int loanTerm) {
+        double interestRate = 0.045; // rate 4.5% 
+        double totalInterest = (loanAmount * interestRate) * loanTerm;
+        double totalPayment = loanAmount + totalInterest;
+        // double payPerMonth = totalPayment / (loanTerm * 12);
+
+        return totalPayment;
     }
 }
