4e83c74ef211d86c1606627ed8d4f06315cf1c32
TunaOfFinality <66070071@kmitl.ac.th>
2024-03-13 19:49:54 +0700

    Update SedanCar, SportCar, SUVCar

--- ShowRoom/src/SUVCar.java
+++ ShowRoom/src/SUVCar.java
@@ -1,3 +1,18 @@
 public class SUVCar extends Car{
+    private String type;
     
+    protected void setType(String type) {
+        this.type = type;
+    }
+    
+    protected String getType() {
+        return this.type;
+    }
+    
+//    @Override
+//    NOT FINISHED
+    public void showDetails() {
+        System.out.println("...........");
+    }
 }
+ 
--- ShowRoom/src/SedanCar.java
+++ ShowRoom/src/SedanCar.java
@@ -1,3 +1,17 @@
 public class SedanCar extends Car{
+    private String type;
     
+    protected void setType(String type){
+        this.type = type;
+    }
+    
+    protected String getType() {
+        return this.type;
+    }
+    
+//    @Override
+//    NOT FINISHED
+    public void showDetails() {
+        System.out.println("...........");
+    }
 }
