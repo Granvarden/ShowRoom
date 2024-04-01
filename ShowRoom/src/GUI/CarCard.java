a47f92882ef53d1d74d1db4a7c381167d91ab9f7
jSSjin <jerat004@gmail.com>
2024-04-01 20:18:22 +0700

    fix card

--- ShowRoom/src/GUI/CarCard.java
+++ ShowRoom/src/GUI/CarCard.java
@@ -116,7 +116,7 @@ public class CarCard extends javax.swing.JPanel {
     }//GEN-LAST:event_detailbuttonActionPerformed
 
     private void BookingButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BookingButtonActionPerformed
-        main.bokingforcard();
+        //main.bokingforcard();
     }//GEN-LAST:event_BookingButtonActionPerformed
 
 
